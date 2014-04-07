c957300ede2fa1b9c4f1095359655fac806600b8
Aggelos Kolaitis <neoaggelos@gmail.com>
2014-04-07 14:31:37 +0300

    [cmake] Added SDLU.framework as a separate target

--- scripts/cmake/Macros.cmake
+++
@@ -1,7 +0,0 @@
-# These are here to keep CMakeLists.txt clean
-
-function(JOIN_FILES FILES GLUE OUTPUT)
-  string(REGEX REPLACE "([^\\]|^);" "\\1${GLUE}" _TMP_STR "${FILES}")
-  string(REGEX REPLACE "[\\](.)" "\\1" _TMP_STR "${_TMP_STR}")
-  set(${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
-endfunction()
