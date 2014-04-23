fd0b1912ad101cb0a9d6b5f5d34ac13b2540ada6
Aggelos Kolaitis <neoaggelos@gmail.com>
2014-04-24 00:33:18 +0300

    Added docs for Combo Box API

--- include/SDLU_combobox.h
+++ include/SDLU_combobox.h
@@ -58,12 +58,18 @@ typedef struct {
 } SDLU_ComboBox;
 
 /**
- *  \brief Create a new emtpy combo box
+ *  \brief Create a new combo box
  */
 extern DECLSPEC SDLU_ComboBox* SDLCALL SDLU_CreateComboBox(SDL_Window* window);
 
 /**
- *  \brief Set geometry
+ *  \brief Set geometry of combo box
+ *
+ *  \param combobox Combo box to change
+ *  \param x, y New position
+ *  \param w, h New size
+ *
+ *  \return 0 on success, -1 on error
  */
 extern DECLSPEC int SDLCALL SDLU_SetComboBoxGeometry(
         SDLU_ComboBox* combobox,
@@ -72,7 +78,12 @@ extern DECLSPEC int SDLCALL SDLU_SetComboBoxGeometry(
 );
 
 /**
- *  \brief Add a new item
+ *  \brief Add a new item to a combo box
+ *
+ *  \param combobox Combo box to add item to
+ *  \param item Item to add
+ *
+ *  \return 0 on success, -1 on error
  */
 extern DECLSPEC int SDLCALL SDLU_AddComboBoxItem(
         SDLU_ComboBox** combobox,
@@ -81,6 +92,11 @@ extern DECLSPEC int SDLCALL SDLU_AddComboBoxItem(
 
 /**
  *  \brief Remove an item
+ *
+ *  \param combobox Combo box to remove item from
+ *  \param item Item to remove
+ *
+ *  \return 0 on success, -1 on error
  */
 extern DECLSPEC int SDLCALL SDLU_DelComboBoxItem(
         SDLU_ComboBox** combobox,
@@ -94,6 +110,29 @@ extern DECLSPEC int SDLCALL SDLU_RenderComboBox(SDLU_ComboBox* combobox);
 
 /**
  *  \brief Set combo box callback
+ *
+ *  The callback function takes two 'void*' arguments, see the docs for
+ *  SDLU_Callback. The first argument is a pointer to the combo box, while the
+ *  second one is userdata. The callback is executed every time a new item is
+ *  chosen from the combo box. A basic callback function should do something
+ *  like this:
+ *
+ *  \code
+ *  void callback_function(void* _this, void* userdata)
+ *  {
+ *      SDLU_ComboBox* this = (SDLU_ComboBox*) this;
+ *
+ *      // Do something, e.g:
+ *      printf("New item: %s\n", this->current);
+ *      printf("New item index: %s\n", this->current_index);
+ *  }
+ *  \endcode
+ *
+ *  \param combobox Combo box to set the callback for
+ *  \param callback The new callback function
+ *  \param arg Optional argument that is passed to the callback function
+ *
+ *  \return 0 on success, -1 on error
  */
 extern DECLSPEC int SDLCALL SDLU_SetComboBoxCallback(
         SDLU_ComboBox* combobox,
