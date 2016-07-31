af4183887218fe3ffb7df3c8e0b614710c4dcb93
Aggelos Kolaitis <neoaggelos@gmail.com>
2016-07-31 09:50:17 -0700

    CMake: also add headers from source code as source files

--- scripts/cmake/CheckFileSystemBackend.cmake
+++ scripts/cmake/CheckFileSystemBackend.cmake
@@ -1,10 +1,10 @@
 macro(CheckFileSystemBackend)
 
   if (WIN32 OR WINDOWS OR CYGWIN)
-    set(FILESYSTEM_BACKEND_SOURCES "${CMAKE_SOURCE_DIR}/src/filesystem/SDLU_filesystem_windows.c")
+    set(FILESYSTEM_BACKEND_SOURCES "${CMAKE_SOURCE_DIR}/src/filesystem/SDLU_filesystem_windows*")
     add_definitions("-DFILESYSTEM_WIN32")
   else()
-    set(FILESYSTEM_BACKEND_SOURCES "${CMAKE_SOURCE_DIR}/src/filesystem/SDLU_filesystem_dirent.c")
+    set(FILESYSTEM_BACKEND_SOURCES "${CMAKE_SOURCE_DIR}/src/filesystem/SDLU_filesystem_dirent*")
     add_definitions("-DFILESYSTEM_DIRENT")
   endif()
   
